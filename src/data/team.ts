4d9d3a04fafcdce60d9e51e56945ea20977eea96
Danil Deineka <tripledpadik@gmail.com>
2023-10-03 16:18:05 +0300

    Add libs + bug fix

--- src/data/team.ts
+++ src/data/team.ts
@@ -3,7 +3,7 @@ import S1x_1_450 from "../images/team/01_s@1x-450.jpg";
 import S2x_1_900 from "../images/team/01_s@2x-900.jpg";
 import M1x_1_354 from "../images/team/01_m@1x-354.jpg";
 import M2x_1_708 from "../images/team/01_m@2x-708.jpg";
-import L1x_1_270 from "../images/team/01_L@1x-270.jpg";
+import L1x_1_270 from "../images/team/01_l@1x-270.jpg";
 import L2x_1_540 from "../images/team/01_l@2x-540.jpg";
 
 // 2
@@ -11,7 +11,7 @@ import S1x_2_450 from "../images/team/02_s@1x-450.jpg";
 import S2x_2_900 from "../images/team/02_s@2x-900.jpg";
 import M1x_2_354 from "../images/team/02_m@1x-354.jpg";
 import M2x_2_708 from "../images/team/02_m@2x-708.jpg";
-import L1x_2_270 from "../images/team/02_L@1x-270.jpg";
+import L1x_2_270 from "../images/team/02_l@1x-270.jpg";
 import L2x_2_540 from "../images/team/02_l@2x-540.jpg";
 
 // 3
@@ -20,7 +20,7 @@ import S1x_3_450 from "../images/team/03_s@1x-450.jpg";
 import S2x_3_900 from "../images/team/03_s@2x-900.jpg";
 import M1x_3_354 from "../images/team/03_m@1x-354.jpg";
 import M2x_3_708 from "../images/team/03_m@2x-708.jpg";
-import L1x_3_270 from "../images/team/03_L@1x-270.jpg";
+import L1x_3_270 from "../images/team/03_l@1x-270.jpg";
 import L2x_3_540 from "../images/team/03_l@2x-540.jpg";
 
 // 4
@@ -28,7 +28,7 @@ import S1x_4_450 from "../images/team/04_s@1x-450.jpg";
 import S2x_4_900 from "../images/team/04_s@2x-900.jpg";
 import M1x_4_354 from "../images/team/04_m@1x-354.jpg";
 import M2x_4_708 from "../images/team/04_m@2x-708.jpg";
-import L1x_4_270 from "../images/team/04_L@1x-270.jpg";
+import L1x_4_270 from "../images/team/04_l@1x-270.jpg";
 import L2x_4_540 from "../images/team/04_l@2x-540.jpg";
 
 export const data = [
